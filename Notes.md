b9e07c0cfe556b5693f58d00eb0245d3d0e6d7ca
Thomas Bullock <motbollox@gmail.com>
2024-09-18 16:58:18 +1000

    Added logger middleware

--- Notes.md
+++ Notes.md
@@ -9,3 +9,13 @@ Installed [REST client](https://marketplace.visualstudio.com/items?itemName=huma
 - finish typescript 😄
 - find github repo for tutorial ✅
 - understand REST client ext ✅
+
+### 18 Sept
+
+https://youtu.be/sFnAHC9lLaw?feature=shared&t=2477
+Added songs endpoint DTO and validation
+Half way through logger middleware
+
+#### TODO
+
+- finish typescript 😄
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // Add your middleware configuration here
+    // For example, you can use the following code to apply a middleware to all routes:
+    consumer.apply(LoggerMiddleware).forRoutes('songs');
+  }
+}
